ebe3bb0034208c4da75f7dae19ed4517ce7d6979
Anthony Schafrik <anthonyschafrik@gmail.com>
2020-05-02 04:14:52 -0400

    Added in some logic going to sleep

--- src/areas/Zulrah/components/PhaseContainer.tsx
+++ src/areas/Zulrah/components/PhaseContainer.tsx
@@ -17,22 +17,68 @@ const PhaseContainer = ({
 
   const totalPhases = Object.keys(phases[rotationKey]).length;
 
+  const handlePhaseUpdate = (key: string, mathType: string) => () =>
+    handlePhaseChange(key, mathType);
+
+  const handleUpdateRotation = (rotation: number) => () =>
+    console.log(rotation);
+
   return (
-    <div>
-      Current Phase {currentPhase}
-      <div>
-        <img src={phases[rotationKey][currentPhase]} alt="Current Phase" />
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <p>Current Phase {currentPhase}</p>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          width: '35%',
+        }}
+      >
+        <img
+          onClick={handleUpdateRotation(1)}
+          src={phases[rotationKey][currentPhase]}
+          alt="Current Phase"
+        />
+
+        {currentPhase !== 1 && currentPhase !== 2 && (
+          <img
+            onClick={handleUpdateRotation(2)}
+            src={phases[rotationKey][currentPhase]}
+            alt="Current Phase"
+          />
+        )}
+
+        {currentPhase !== 1 && (
+          <img
+            onClick={handleUpdateRotation(3)}
+            src={phases[rotationKey][currentPhase]}
+            alt="Current Phase"
+          />
+        )}
+
+        {currentPhase !== 1 && (
+          <img
+            onClick={handleUpdateRotation(4)}
+            src={phases[rotationKey][currentPhase]}
+            alt="Current Phase"
+          />
+        )}
       </div>
       <div>
         <button
           disabled={currentPhase === 1 ? true : false}
-          onClick={() => handlePhaseChange('currentPhase', 'mines')}
+          onClick={handlePhaseUpdate('currentPhase', 'mines')}
         >
           {'<='}
         </button>
         <button
           disabled={totalPhases === currentPhase ? true : false}
-          onClick={() => handlePhaseChange('currentPhase', 'add')}
+          onClick={handlePhaseUpdate('currentPhase', 'add')}
         >
           {'=>'}
         </button>
